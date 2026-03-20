bd9837dda74cd1aafb9bfa3867ae6a7ddcd8b9a8
jiming <231880303@smail.nju.edu.cn>
2026-03-20 15:43:13 +0800

    加入初始版本BPE

---
+++ assignment1-basics/cs336_basics/BPETokenizer_v1.py
@@ -0,0 +1,70 @@
+# 最初等的BPE实现
+# 维护vocab: dict[int, bytes], merges: list[tuple[bytes,bytes]]
+# 在训练的过程中维护 stats: dict[tuple[bytes, bytes], int]统计每次训练后字节对的计数
+
+import regex as re
+import os
+
+
+class BPETokenizer():
+    
+    def __init__(self):
+        self.vocab = dict[int, bytes] = {}
+        self.merges = list[tuple(bytes, bytes)] = []
+        self.pre_tokenization_pattern = re.compile(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+        self.spilt_pattern = "|".join(re.escape(token) for token in special_tokens)
+        
+    
+    def _train_init_vocab(self, special_tokens):
+        special_vocab = {i: st.encode("utf-8") for i ,st in enumerate(special_tokens)}
+        initial_vocab = {i + len(special_vocab): bytes([i]) for i in range(256)}
+        return special_vocab | initial_vocab
+    
+    def _train_get_stats(self, token, stats):
+        for pair in zip(token, token[1:]):
+            stats[pair] = stats.get(pair, 0) + 1
+        return stats
+    
+    def _train_merge(self, token, pair):
+        new_token = []
+        i = 0
+        while i < len(token):
+            if token[i] == pair[0] and i < len(token) - 1 and token[i + 1] == pair[1]:
+                new_token.append(pair[0] + pair[1])
+                i += 2
+            else:
+                new_token.append(token[i])
+                i += 1
+            
+        return new_token
+            
+    def train(self, input_path, vocab_size, special_tokens):
+        
+        with open(input_path, "rb") as f:
+            corpus = f.read().decode("utf-8", errors="ignore")
+            
+        spilt_corpus = re.split(self.spilt_pattern, corpus)
+        pre_tokens = [] # list[list[bytes, ...]]
+        for text in spilt_corpus:
+            text = re.findall(self.pre_tokenization_pattern, text)
+            pre_tokens.extend([[bytes(ch, "utf-8") for ch in tk] for tk in text])
+            
+        self.vocab = self._train_init_vocab(special_tokens)
+        init_size = len(self.vocab)
+        num_merge = vocab_size - init_size
+        
+        for i in range(num_merge):
+            stats = {} # dict[tuple[bytes, bytes], int]
+            for tk in pre_tokens:
+                self._train_get_stats(tk, stats)
+                
+            pair = max(stats.items(), key=lambda x: (x[1], x[0]))
+            pre_tokens = [self._train_merge(tk, pair) for tk in pre_tokens]
+            
+            idx = i + init_size
+            self.merges.append(pair)
+            self.vocab[idx] = pair[0] + pair[1]
+        
+        return self.vocab, self.merges
+            
+        
